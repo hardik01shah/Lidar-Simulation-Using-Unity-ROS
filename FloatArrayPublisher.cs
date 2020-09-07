45f5d383b5970be343d7490abcba5cd96868ff85
hardik01shah <61026273+hardik01shah@users.noreply.github.com>
2020-09-07 17:52:49 +0530

    Float Array Publisher Script
    This C# script is responsible for publishing the required float array to a ROS topic.

---
+++ FloatArrayPublisher.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class FloatArrayPublisher : Publisher<Messages.Float32MultiArray>
+    {
+        private Messages.Float32MultiArray message;
+
+        protected override void Start()
+        {
+            base .Start();
+            InitializeMessage();
+        }
+
+        private void InitializeMessage()
+        {
+            message = new Messages.Float32MultiArray
+            {
+                data = lidarfinal.distances
+            };
+        }
+
+        private void Update()
+        {
+            message.data = lidarfinal.distances;
+            Publish(message);
+        }
+    }
+}
