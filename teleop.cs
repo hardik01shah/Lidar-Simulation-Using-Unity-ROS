70d0ddf7a650f70501411a73a5b651684300c5c8
hardik01shah <61026273+hardik01shah@users.noreply.github.com>
2020-09-07 17:56:17 +0530

    teleop script for teleoperation of lidar
    Aattch this C# script to your LIDAR game object to teleoperate the lidar using arrow keys

---
+++ teleop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleop : MonoBehaviour
+{
+    public float speed;
+
+	void Update() {
+		float horizontal= Input.GetAxisRaw("Horizontal");
+    	float vertical= Input.GetAxisRaw("Vertical");
+
+    	Vector3 direction = new Vector3(horizontal, 0, vertical);
+    	gameObject.transform.Translate(direction.normalized * Time.deltaTime *speed);
+	}
+}
